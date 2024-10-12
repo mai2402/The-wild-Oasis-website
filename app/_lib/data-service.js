14220a00697c2a5fbdfbd949f2862a2851c3bfb4
mai2402 <maifareed242@gmail.com>
2024-10-12 13:00:43 +0300

    setting up error boundries

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from 'date-fns';
 import { supabase } from './supabase';
+import { notFound } from 'next/navigation';
 /////////////
 // GET
 
@@ -15,6 +16,7 @@ export async function getCabin(id) {
 
   if (error) {
     console.error(error);
+    notFound()
   }
 
   return data;
@@ -29,6 +31,7 @@ export async function getCabinPrice(id) {
 
   if (error) {
     console.error(error);
+   
   }
 
   return data;
