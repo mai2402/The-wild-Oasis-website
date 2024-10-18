1db57b1490d5a4543ba20799e60becb0b0998f37
mai2402 <maifareed242@gmail.com>
2024-10-18 09:30:54 +0300

    useFormStauts to display updating indecation

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,6 +1,7 @@
 "use client"
 
 import { updateGuestProfile } from "../_lib/actions"
+import {useFormStatus} from "react-dom"
 
 function UpdateProfileForm({guest,children}) {
     const {fullName,email, nationality, nationalID,countryFlag}= guest
@@ -51,12 +52,22 @@ function UpdateProfileForm({guest,children}) {
         </div>
 
         <div className="flex justify-end items-center gap-6">
-          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-            Update profile
-          </button>
+            <Button/>
         </div>
       </form>
     )
 }
 
 export default UpdateProfileForm
+
+
+function Button (){
+  const {pending}= useFormStatus()
+
+  return(
+    <button disabled ={pending}
+            className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+    {pending?"....Updating" :"Update profile"}
+  </button>
+  )
+}
