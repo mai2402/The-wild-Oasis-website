cca33aed2bd57e1b0340072b740badce0149ddd6
mai2402 <maifareed242@gmail.com>
2024-10-19 10:43:32 +0300

    deleting a resrvation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,28 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
+
+
+export async function deleteReservation(bookingId){
+  const session = await auth();
+   
+  if (!session )
+   throw new Error("You must be logged in!!")
+
+  const guestBookings = await getBookings(session.guestId)
+  const guestBookingsId = guestBookings.map((booking)=>booking.id);
+
+  if(!guestBookingsId.includes(bookingId)) 
+    throw new Error("you are not allowed to delete this booking!!")
+
+  const {  error } = 
+  await supabase.from('bookings').delete().eq('id', bookingId);
+
+  if (error) throw new Error('Booking could not be deleted');
+ 
+   revalidatePath("/account/reservations")
+}
 
 export async function updateGuestProfile(formData){
    const session = await auth();
